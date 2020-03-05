39b074efe3cfc8772f0cb842f36e9d70aaa2433b
Andriulia <andriuliaandriulia@gmail.com>
2020-03-05 06:43:54 +0200

    Update CarWheel.java
    Final :)

--- GoJava15_HW5/src/Main/Java/Car/CarWheel.java
+++ GoJava15_HW5/src/Main/Java/Car/CarWheel.java
@@ -1,35 +1,35 @@
 package Main.Java.Car;
 
-public class CarWheel {
+class CarWheel {
     private float tyreStatus;
 
-    public CarWheel() {
+    CarWheel() {
         this.tyreStatus = 1f;
     }
 
-    public CarWheel(float tyreStatus) {
+    CarWheel(float tyreStatus) {
         this.tyreStatus = tyreStatus;
     }
 
-    public void tyreChange() {
+    void tyreChange() {
         this.tyreStatus = 1f;
     }
 
-    public void tyreWear(int newWearing) {
-            this.tyreStatus -= (float) newWearing / 100;
+    void tyreWear(int newWearing) {
+        this.tyreStatus -= (float) newWearing / 100;
     }
 
-    public float getTyreStatus() {
+    float getTyreStatus() {
         return this.tyreStatus;
     }
 
-    public void wheelInfo() {
-        if (tyreStatus == 0.0f) {
+    void wheelInfo() {
+        if (tyreStatus * 100 < 1f) {
             System.out.println("The tyre is worn at all.");
         } else if (tyreStatus == 1f) {
             System.out.println("The tyre is not worn at all.");
         } else {
-            System.out.println("The tyre is worn by " + (int) (100 - tyreStatus*100) + " percents.");
+            System.out.println("The tyre is worn by " + (int) (100 - tyreStatus * 100) + " percents.");
         }
     }
 }
