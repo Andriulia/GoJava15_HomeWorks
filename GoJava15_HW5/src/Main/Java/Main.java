f11224ed1833533e71403d4677dfb323d439b882
Andriulia <andriuliaandriulia@gmail.com>
2020-03-05 06:34:21 +0200

    Update Main.java
    Final :)

--- GoJava15_HW5/src/Main/Java/Main.java
+++ GoJava15_HW5/src/Main/Java/Main.java
@@ -3,25 +3,10 @@ package Main.Java;
 import Main.Java.Car.*;
 
 public class Main {
-	public static void main(String[] args) {
-		/*CarDoor r1 = new CarDoor();
-		r1.windowChanged();
-		r1. doorInfo();
-		CarDoor r2 = new CarDoor(Status.OPENED, Status.OPENED);
-		r2.windowChanged();
-		r2.doorInfo();
-		CarWheel l1 = new CarWheel(65);
-		CarWheel l2 = new CarWheel();
-		l2.tyreWearing(0);
-		l1.wheelInfo();
-		l2.wheelInfo();*/
-		
-		Car car = new Car("car", 1, "1", 130, 3, 4, 5, 6, 4);
-		//car.getWheels().get(2).tyreWear(21);
-		//System.out.println(car.getWheels().get(2).getTyreStatus());
-		//System.out.println();
-		//System.out.println(car.currentMaxSpeed());
-		//car.carInfo();
-		car.carInterface();
-	}
+    public static void main(String[] args) {
+
+        Car car = new Car("car", 1, "1", 130, 3,
+                8, 5, 6, 4);
+        car.carInterface();
+    }
 }
